cd9237846e0d2a901cf9027bab0a0d9c38fbaf27
IcyPow <alexandr.dolzhkov@gmail.com>
2023-03-30 01:33:56 +0300

    Потоки

--- udpclient.py
+++ udpclient.py
@@ -1,6 +1,12 @@
 import socket
 import sys
+import threading
 client=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+locker=threading.Lock()
 while True:
     msg=input()
     client.sendto(msg.encode('utf-8'), (sys.argv[1], int(sys.argv[2])))
+    locker.acquire()
+    msg=client.recv(1024).decode('utf-8')
+    locker.release()
+    print(msg)
--- udpserver.py
+++ udpserver.py
@@ -1,12 +1,13 @@
 import socket
 import sys
+import threading
 import queue
-import re
+locker=threading.Lock()
 queues=dict()
 connections=list()
 server=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(((sys.argv[1]), int(sys.argv[2])))
-while True:
+def q():
     msg, adr=server.recvfrom(1024)
     msg = msg.decode('utf-8')
     if adr not in connections:
@@ -15,7 +16,25 @@ while True:
     iterator=msg.find('+')
     if iterator != -1:
         queue_name, queue_msg = msg[:iterator], msg[iterator+1:]
-        print(queue_name, queue_msg, "В очередь")
+        print("Сообщение", queue_msg, "от", adr, "в очередь", queue_name)
+        if queue_name not in queues:
+            locker.acquire()
+            print("Создана новая очередь", queue_name)
+            queues[queue_name]=queue.Queue()
+            locker.release()
+        queues[queue_name].join
+        server.sendto("Successfully added".encode('utf-8'), adr)
+        queues[queue_name].put(queue_msg)
     else:
         queue_name=msg
-        print(queue_name, "Из очереди")
+        if queue_name not in queues:
+            server.sendto(("SERVER NOTIFICATION: Нет такой очереди").encode('utf-8'), adr)
+            return
+        print(adr, "берет из очереди", queue_name, )
+        queues[queue_name].join
+        server.sendto(queues[queue_name].get(block=True).encode('utf-8'), adr)
+        if queues[queue_name].empty()==True:
+            print("Удаление очереди", queue_name)
+            del queues[queue_name]
+while True:
+    t1=threading.Thread(target=q()).start()
