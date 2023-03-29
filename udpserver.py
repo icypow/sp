f5bb0a8c1bd20d4973948f48ad7f68369361d035
IcyPow <alexandr.dolzhkov@gmail.com>
2023-03-29 13:21:24 +0300

    Парсим

--- udpserver.py
+++ udpserver.py
@@ -1,12 +1,21 @@
 import socket
 import sys
+import queue
+import re
+queues=dict()
 connections=list()
 server=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(((sys.argv[1]), int(sys.argv[2])))
 while True:
     msg, adr=server.recvfrom(1024)
+    msg = msg.decode('utf-8')
     if adr not in connections:
         print(adr, "connected")
         connections.append(adr)
-    print(msg.decode('utf-8'))
-#server.sendto("Hello, client!".encode('utf-8'), adr)
+    iterator=msg.find('+')
+    if iterator != -1:
+        queue_name, queue_msg = msg[:iterator], msg[iterator+1:]
+        print(queue_name, queue_msg, "В очередь")
+    else:
+        queue_name=msg
+        print(queue_name, "Из очереди")
