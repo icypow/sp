2978121da61ffb0504bb3e61b5af00a578903ddc
IcyPow <alexandr.dolzhkov@gmail.com>
2023-04-03 15:03:47 +0300

    Пакетированная отправка сообщений

--- udpclient.py
+++ udpclient.py
@@ -3,10 +3,32 @@ import sys
 import threading
 import hashlib
 import time
+import struct
 client=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 locker=threading.Lock()
 client.settimeout(5)
 i=5
+def make_packet(data):
+    packet_len = len(data)
+    checksum = hashlib.md5(data).digest()
+    packet = struct.pack('!I16s', packet_len, checksum[:16]) + data[:1000]
+    return packet
+def unpack_packet(socket, adr):
+    packet=socket.recv(1024)
+    packet_len, checksum = struct.unpack('!I16s', packet[:20])
+    data = packet[20:20+packet_len]
+    if data.decode('utf-8')[:6]=="SERVER":
+        return data.decode('utf-8'), packet_len
+    if hashlib.md5(data).digest()[:16] != checksum:
+        while True:
+            socket.sendto("1".encode('utf-8'), adr)
+            packet=socket.recv(1024)
+            packet_len, checksum = struct.unpack('!I16s', packet[:20])
+            data = packet[20:20+packet_len]
+            if hashlib.md5(data).digest()[:16] == checksum:
+                break
+    socket.sendto("0".encode('utf-8'), adr)
+    return data.decode('utf-8'), packet_len 
 name=sys.argv[3]
 client.sendto((name).encode('utf-8'), (sys.argv[1], int(sys.argv[2])))
 try:
@@ -19,40 +41,27 @@ if response=="0":
     exit()
 while True:
     msg=input()
-    checksum = hashlib.md5(msg.encode('utf-8')).hexdigest()
-    msg=str(checksum)+msg
+    msg=msg.encode('utf-8')
+    while msg:
+        packet=make_packet(msg)
+        while True:
+            client.sendto(packet, (sys.argv[1], int(sys.argv[2])))
+            try:
+                response=client.recv(1024).decode('utf-8')
+            except socket.timeout:
+                print('Превышено время ожидания ответа сервера1')
+                exit()
+            if response=="0":
+                break
+        msg=msg[1000:]
+    response=""
     while True:
-        # if i>0:
-        #     client.sendto((hashlib.md5("random".encode('utf-8')).hexdigest()+"123+random").encode('utf-8'), (sys.argv[1], int(sys.argv[2])))
-        #     i-=1
-        # else:
-        client.sendto((msg).encode('utf-8'), (sys.argv[1], int(sys.argv[2])))
         try:
-            response=client.recv(1024).decode('utf-8')
+            resp, leng = unpack_packet(client, (sys.argv[1], int(sys.argv[2])))
         except socket.timeout:
-            print('Превышено время ожидания ответа сервера1')
-            exit()
-        if response=="0":
+                print('Превышено время ожидания ответа сервера2')
+                exit()
+        response+=resp
+        if leng<=1000:
             break
-    try:
-        response=client.recv(1024).decode('utf-8')
-    except socket.timeout:
-            print('Превышено время ожидания ответа сервера2')
-            exit()
-    if response[:6]=="SERVER":
-        print(response)
-    else:
-        msg=response[32:]
-        income_checksum=response[:32]
-        checksum = hashlib.md5(msg.encode('utf-8')).hexdigest()
-        while income_checksum!=checksum:
-            print("UDP-INFO: Чексумма не совпала. Оповещаю отправителя и жду переотправки пакета")
-            client.sendto("1".encode('utf-8'), (sys.argv[1], int(sys.argv[2])))
-            response=client.recv(1024).decode('utf-8') 
-            msg=response[32:]
-            income_checksum=response[:32]
-            checksum = hashlib.md5(msg.encode('utf-8')).hexdigest()
-        print("UDP-INFO: Чексумма совпала! Беру сообщения в оборот")
-        #time.sleep(10)
-        client.sendto("0".encode('utf-8'), (sys.argv[1], int(sys.argv[2])))
-        print(msg)
+    print(response)
--- udpserver.py
+++ udpserver.py
@@ -4,39 +4,66 @@ import threading
 import queue
 import hashlib
 import time
+import struct
 max_timeout=5
 locker=threading.Lock()
 queues=dict()
 server=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(((sys.argv[1]), int(sys.argv[2])))
 connections=list()
-names=list()
+names=dict()
 recv_list=list()
 recv_dict=dict()
-
+def make_packet(data):
+    packet_len = len(data)
+    checksum = hashlib.md5(data).digest()
+    packet = struct.pack('!I16s', packet_len, checksum[:16]) + data[:1000]
+    return packet
+def unpack_packet(socket, adr, packet):
+    packet_len, checksum = struct.unpack('!I16s', packet[:20])
+    data = packet[20:20+packet_len]
+    if hashlib.md5(data).digest()[:16] != checksum:
+        while True:
+            socket.sendto("1".encode('utf-8'), names[adr])
+            recv_list.append(adr)
+            set_timer=time.time()
+            while adr in recv_list:
+                if time.time()-set_timer>max_timeout:
+                    print("Превышено время ожидания от", names[adr])
+                    recv_list.remove(adr)
+                    connections.remove(adr)
+                    return
+            packet=recv_dict[adr]
+            packet_len, checksum = struct.unpack('!I16s', packet[:20])
+            data = packet[20:20+packet_len]
+            if hashlib.md5(data).digest()[:16] == checksum:
+                break
+    socket.sendto("0".encode('utf-8'), adr)
+    return data.decode('utf-8'), packet_len 
 def q(adr, msg):
     global recv_dict
     global recv_list
-    income_checksum=msg[:32]
-    msg=msg[32:]
-    checksum = hashlib.md5(msg.encode('utf-8')).hexdigest()
-    while income_checksum!=checksum:
-        print("Чексумма не совпала. Оповещаю отправителя и жду переотправки пакета")
-        server.sendto("1".encode('utf-8'), adr)
-        recv_list.append(adr)
-        set_timer=time.time()
-        while adr in recv_list:
-            if time.time()-set_timer>max_timeout:
-                print("Превышено время ожидания от", adr)
-                recv_list.remove(adr)
-                connections.remove(adr)
-                return
-        response=recv_dict[adr]
-        msg=response[32:]
-        income_checksum=response[:32]
-        checksum = hashlib.md5(msg.encode('utf-8')).hexdigest()
-    print("UDP-INFO: Чексумма совпала! Беру сообщения в оборот")
-    server.sendto("0".encode('utf-8'), adr)
+    response=""
+    while True:
+        try:
+            resp, leng = unpack_packet(server, adr, msg)
+        except socket.timeout:
+                print('Превышено время ожидания ответа клиента', names[adr])
+                exit()
+        response+=resp
+        if leng<=1000:
+            break
+        else:
+            recv_list.append(adr)
+            set_timer=time.time()
+            while adr in recv_list:
+                if time.time()-set_timer>max_timeout:
+                    print("Превышено время ожидания от", names[adr])
+                    recv_list.remove(adr)
+                    connections.remove(adr)
+                    return
+            msg=recv_dict[adr]
+    msg=response
     iterator=msg.find('+')
     if iterator != -1:
         queue_name, queue_msg = msg[:iterator], msg[iterator+1:]
@@ -45,47 +72,53 @@ def q(adr, msg):
             print("Создана новая очередь", queue_name)
             queues[queue_name]=queue.Queue()
         locker.release()
-        print("Сообщение '", queue_msg, "' от ", adr, " в очередь ", queue_name, sep='')
+        print("Сообщение '", queue_msg, "' от ", names[adr], " в очередь ", queue_name, sep='')
         locker.acquire()
         queues[queue_name].join
         try:
             queues[queue_name].put(queue_msg)
         except:
-            server.sendto(("SERVER: Невозможно невозможно положить в очередь").encode('utf-8'), adr)
+            server.sendto(make_packet(("SERVER: Невозможно невозможно положить в очередь").encode('utf-8')), adr)
             return
         finally:
-            server.sendto("SERVER: Успешное добавление в очередь".encode('utf-8'), adr)
+            server.sendto(make_packet(("SERVER: Успешное добавление в очередь").encode('utf-8')), adr)
         locker.release()
     else:
         queue_name=msg
         if queue_name not in queues:
-            server.sendto(("SERVER: Нет такой очереди").encode('utf-8'), adr)
+            server.sendto(make_packet(("SERVER: Нет такой очереди").encode('utf-8')), adr)
             return
-        print(adr, "берет из очереди", queue_name, )
+        print(names[adr], "берет из очереди", queue_name, )
         locker.acquire()
         queues[queue_name].join
         try:
             msg=queues[queue_name].get(block=True)
         except:
-            server.sendto(("SERVER: Невозможно получить из очереди").encode('utf-8'), adr)
+            server.sendto(make_packet(("SERVER: Невозможно получить из очереди").encode('utf-8')), adr)
             return
         locker.release()
-        checksum = hashlib.md5(msg.encode('utf-8')).hexdigest()
-        msg=str(checksum)+msg
-        print("Попытка послать сообщение клиенту")
-        while True:
-            server.sendto(msg.encode('utf-8'), adr)
-            recv_list.append(adr)
-            set_timer=time.time()
-            while adr in recv_list:
-                if time.time()-set_timer>max_timeout:
-                    print("Превышено время ожидания от", adr)
-                    recv_list.remove(adr)
-                    connections.remove(adr)
-                    return
-            if recv_dict[adr]=="0":
-                print("Успешно!")
-                break
+        print("Попытка послать сообщение клиенту", names[adr])
+        while msg:
+            packet=make_packet(msg.encode('utf-8'))
+            while True:
+                server.sendto(packet, adr)
+                try:
+                    recv_list.append(adr)
+                    set_timer=time.time()
+                    while adr in recv_list:
+                        if time.time()-set_timer>max_timeout:
+                            print("Превышено время ожидания от", names[adr])
+                            recv_list.remove(adr)
+                            connections.remove(adr)
+                            return
+                    response=recv_dict[adr]
+                except socket.timeout:
+                    print('Превышено время ожидания ответа сервера1')
+                    exit()
+                if response.decode('utf-8')=="0":
+                    break
+            msg=msg[1000:]
+        print("Успешная отправка клиенту", names[adr])
         locker.acquire()
         if queues[queue_name].empty()==True:
             print("Удаление очереди", queue_name)
@@ -93,23 +126,23 @@ def q(adr, msg):
         locker.release()
 while True:
     msg, adr=server.recvfrom(1024)
-    print(adr)
     if adr not in connections:
         name=msg.decode('utf-8')
         if name in names:
             server.sendto("0".encode('utf-8'), adr)
             continue
         server.sendto("1".encode('utf-8'), adr)
-        print(name, "connected")
+        print(name, "подключился")
         connections.append(adr)
-        names.append(name)
+        names[adr]=name
         continue
-    msg = msg.decode('utf-8')
-    #print("FIRST IN", msg)
     if adr in recv_list:
         recv_dict[adr]=msg
         recv_list.remove(adr)
-    if (msg=="0" or msg=="1")and adr not in recv_list:
         continue
     else:
+        # packet=msg
+        # packet_len, checksum = struct.unpack('!I16s', packet[:20])
+        # data = packet[20:20+packet_len]
+        # print(data.decode('utf-8'))
         t = threading.Thread(target=q, args=(adr, msg)).start()
